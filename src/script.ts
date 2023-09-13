706d2985846e5af5fd6e0fc52adf364acb9d1fbe
Light <sagarofficial2@gmail.com>
2023-09-13 19:40:16 +0530

    implemented classes and interfaces

--- src/script.ts
+++ src/script.ts
@@ -1,4 +1,20 @@
-const form = document.querySelector('.new-item-form') as HTMLFormElement
+import { Invoice } from './classes/invoice.js';
+import { Payment } from './classes/payment.js';
+import { HasFormatter } from './interfaces/hasFormatter.js';
+
+// let docOne: HasFormatter;
+// let docTwo: HasFormatter;
+
+// docOne = new Invoice('yoshi', 'web work', 250);
+// docTwo = new Payment('mario', 'plumbing', 200);
+
+// let docs: HasFormatter[] = [];
+// docs.push(docOne);
+// docs.push(docTwo);
+
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+console.log(form.children);
+
 // inputs
 const type = document.querySelector('#type') as HTMLInputElement;
 const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
@@ -6,9 +22,13 @@ const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
 
 form.addEventListener('submit', (e: Event) => {
-    e.preventDefault();
-    console.log(type.value);
-    console.log(details.value);
-    console.log(amount.valueAsNumber);
-    console.log(tofrom.value);
-})
+  e.preventDefault();
+
+  let doc: HasFormatter;
+  if (type.value === 'invoice') {
+    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+  } else {
+    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+  }
+  console.log(doc);
+});
