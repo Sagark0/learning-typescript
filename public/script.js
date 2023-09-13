607912cb412c00a2cce17535ab1e7ee0e8e07862
Light <sagarofficial2@gmail.com>
2023-09-13 16:51:43 +0530

    dom and type casting of dom

--- public/script.js
+++ public/script.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log("testing");
+const form = document.querySelector('.new-item-form');
+// inputs
+const type = document.querySelector('#type');
+const tofrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(type.value);
+    console.log(details.value);
+    console.log(amount.valueAsNumber);
+    console.log(tofrom.value);
+});
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,14 @@
-console.log("testing")
+const form = document.querySelector('.new-item-form') as HTMLFormElement
+// inputs
+const type = document.querySelector('#type') as HTMLInputElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+form.addEventListener('submit', (e: Event) => {
+    e.preventDefault();
+    console.log(type.value);
+    console.log(details.value);
+    console.log(amount.valueAsNumber);
+    console.log(tofrom.value);
+})
